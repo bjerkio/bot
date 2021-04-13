6a08a565c9d534468f9e43c8b13b919e29dccd20
Simen A. W. Olsen <cobraz@cobraz.no>
2021-04-13 23:50:02 +0200

    chore: Add BjerkBot Token (#1)

--- repositories.ts
+++ repositories.ts
@@ -1,3 +1,8 @@
-export const repositories = [
-  { repo: 'bjerkio/bot', invitationId: 44182036 },
+export interface Repository {
+  repo: string;
+  invitationId?: number;
+  token?: boolean;
+}
+export const repositories: Repository[] = [
+  // { repo: 'bjerkio/bot', invitationId: 44182036, token: true },
 ];

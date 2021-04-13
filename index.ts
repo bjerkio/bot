d712967ee98800c16a79e0ecb5f82222dbe1e8e8
Simen A. W. Olsen <so@bjerk.io>
2021-04-13 23:46:13 +0200

    chore: Add BjerkBot Token

--- index.ts
+++ index.ts
@@ -1,10 +1,41 @@
-// import * as pulumi from '@pulumi/pulumi';
+import * as pulumi from '@pulumi/pulumi';
 import * as github from '@pulumi/github';
 import { repositories } from './repositories';
 
-repositories.map(
-  ({ repo, invitationId }) =>
-    new github.UserInvitationAccepter(repo, {
+const config = new pulumi.Config();
+
+const invites = repositories
+  .filter(r => r.token)
+  .map(({ repo, invitationId }) => {
+    return new github.UserInvitationAccepter(repo, {
       invitationId: String(invitationId),
-    }),
-);
+    });
+  });
+
+const reposWithToken = repositories.filter(r => r.token);
+const providers = new Map();
+
+reposWithToken.map(({repo}) => {
+  const [org] = repo.split('/');
+  if (!providers.has(org)) {
+    providers.set(
+      org,
+      new github.Provider(org, {
+        organization: org,
+      }),
+    );
+  }
+});
+
+reposWithToken.map(({ repo }) => {
+  const [org, repository] = repo.split('/');
+  return new github.ActionsSecret(
+    repository,
+    {
+      secretName: 'BJERKBOT_GITHUB_TOKEN',
+      plaintextValue: config.requireSecret('bjerkbot-github-token'),
+      repository,
+    },
+    { provider: providers.get(org), dependsOn: invites },
+  );
+});

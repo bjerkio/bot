a25d80fcab89f4cd19b4be52622d1a9cfb236db0
Simen A. W. Olsen <so@bjerk.io>
2023-07-14 18:50:19 +0200

    refactor: use packages and improved drift checking

--- github/bjerkorg/provider.ts
+++
@@ -1,7 +0,0 @@
-import * as github from '@pulumi/github';
-import { githubToken } from '../../config';
-
-export const provider = new github.Provider('bjerkorg', {
-  owner: 'bjerkio',
-  token: githubToken,
-});

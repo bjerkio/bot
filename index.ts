4e1c9f1dce30d8e405d33fc1b57178542115a712
Simen A. W. Olsen <so@bjerk.io>
2023-08-11 15:59:10 +0200

    export github token (#30)

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as github from './resources/github';
 import './resources/bjerkorg';
 import './resources/getbranches';
 import './resources/github';
 import './resources/npm';
+
+export const gitHubToken = pulumi.secret(github.githubToken);

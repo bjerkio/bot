ee5a627b2162abc9f1617919f0572732315431d6
Brage Sekse Aarset <brage@bjerk.io>
2024-01-10 09:51:29 +0100

    feat: add branches token, remove getGithubProvider function (#45)
    * feat: add branches token, remove getGithubProvider function
    
    * fix: remove dead import
    
    * fix: remove unused import

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-import * as pulumi from '@pulumi/pulumi';
-import * as github from './resources/github';
 import './resources/bjerkorg';
 import './resources/getbranches';
-import './resources/github';
 import './resources/npm';
-
-export const githubToken = pulumi.secret(github.githubToken);
--- resources/getbranches.ts
+++ resources/getbranches.ts
@@ -1,14 +1,22 @@
 import * as github from '@pulumi/github';
-import { githubToken, getGithubProvider } from './github';
+import { getToken } from 'get-pulumi-secret';
 
-const provider = getGithubProvider('getbranches');
+export const branchesGitHubToken = getToken({
+  name: 'getbranches-token',
+  namespace: 'github',
+});
+
+export const branchesGitHubProvider = new github.Provider('getbranches', {
+  owner: 'getbranches',
+  token: branchesGitHubToken,
+});
 
 new github.ActionsOrganizationSecret(
   'getbranches-github-token',
   {
-    plaintextValue: githubToken,
+    plaintextValue: branchesGitHubToken,
     secretName: 'BJERKBOT_GITHUB_TOKEN',
     visibility: 'all',
   },
-  { provider },
+  { provider: branchesGitHubProvider },
 );

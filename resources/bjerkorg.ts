a7890d087daa5184423281349d60996e7cb9d4b3
Brage Sekse Aarset <brage@bjerk.io>
2024-01-10 10:32:05 +0100

    fix: mark tokens as secrets

--- resources/bjerkorg.ts
+++ resources/bjerkorg.ts
@@ -1,11 +1,14 @@
 import * as github from '@pulumi/github';
+import * as pulumi from '@pulumi/pulumi';
 import { npmToken } from './npm';
 import { getToken } from 'get-pulumi-secret';
 
-export const bjerkGitHubToken = getToken({
-  name: 'bjerkio-token',
-  namespace: 'github',
-});
+export const bjerkGitHubToken = pulumi.secret(
+  getToken({
+    name: 'bjerkio-token',
+    namespace: 'github',
+  }),
+);
 
 export const bjerkGitHubProvider = new github.Provider('bjerkio', {
   owner: 'bjerkio',
--- resources/getbranches.ts
+++ resources/getbranches.ts
@@ -1,10 +1,13 @@
 import * as github from '@pulumi/github';
+import * as pulumi from '@pulumi/pulumi';
 import { getToken } from 'get-pulumi-secret';
 
-export const branchesGitHubToken = getToken({
-  name: 'getbranches-token',
-  namespace: 'github',
-});
+export const branchesGitHubToken = pulumi.secret(
+  getToken({
+    name: 'getbranches-token',
+    namespace: 'github',
+  }),
+);
 
 export const branchesGitHubProvider = new github.Provider('getbranches', {
   owner: 'getbranches',

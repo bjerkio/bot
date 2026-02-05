2c62b3649cbcb076059797771f943a7c1b11957f
Brage Sekse Aarset <brage@bjerk.io>
2026-02-05 16:38:33 +0100

    fix: path reference

--- resources/npm.ts
+++ resources/npm.ts
@@ -1,6 +1,6 @@
 import { getToken } from './op-secret.js';
 import * as pulumi from '@pulumi/pulumi';
 
-const config = new pulumi.Config();
+const config = new pulumi.Config('op');
 
-export const npmToken = await getToken(config.require('npm-token'));
+export const npmToken = await getToken(config.require('npm-token-path'));
